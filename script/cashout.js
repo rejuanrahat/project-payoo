680e0d53f96a0bfc2ca3f0ecf40bad4636007c55
Your Name <your-email@example.com>
2026-02-21 03:34:29 +0600

    made cashout section structure properly and ready to make it functional

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById("cashout-btn").addEventListener("click", function() {
+    // 1- get the agent number & validate
+    // 2- get the amount, validate, convart to number
+    // 3- get the current balance , validate, convart to number
+    // 4- calculate the new balance
+    // 5- get the pin, validate
+    // 5.1- true => alert> cashout successful! new balance is
+    // 5.1- false => alert> invalid pin! return to home page
+})
