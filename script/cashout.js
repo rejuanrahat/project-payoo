325ac96e0f27dc4b416d01ea9502dfa5d0f66724
Your Name <your-email@example.com>
2026-02-21 22:12:10 +0600

    created add money form and ready to make it functional

--- script/cashout.js
+++ script/cashout.js
@@ -2,39 +2,34 @@ document.getElementById("cashout-btn").addEventListener("click", function () {
 
     // 1- get the agent number [& validate]
 
-    const cashoutNumberInput = document.getElementById("cashout-number");
-    const cashoutNumber = cashoutNumberInput.value;
+        const cashoutNumber = getValueFromInput("cashout-number");
     if (cashoutNumber.length !== 11){
         alert("Invalid agent number! Please enter a valid 11-digit agent number.");
     }
 
     // 2- get the amount, [validate, convart to number]
 
-    const cashoutAmountInput = document.getElementById("cashout-amount");
-    const cashoutAmount = parseFloat(cashoutAmountInput.value);
+    const cashoutAmount = getValueFromInput("cashout-amount");
 
-    // 3- get the current balance [, validate, convart to number]
+    // 3- get the current balance [, validate, convart to number];
 
-    const balanceElement = document.getElementById("balance");
-    const balance = balanceElement.innerText;
+    const currentBalance = getBalance();
 
     // 4- calculate the new balance
 
-    const newBalance = Number(balance) - Number(cashoutAmount);
-
+    const newBalance = currentBalance - Number(cashoutAmount);
     if(newBalance < 0) {
         alert("Insufficient balance! Your current balance is " + balance);
         return;
     }
     // 5- get the pin, validate
 
-    const cashoutPinInput = document.getElementById("cashout-pin");
-    const pin = cashoutPinInput.value;
+    const pin = getValueFromInput("cashout-pin");
     if (pin === "6251") {
 
         // 5.1- true => alert> cashout successful! new balance is
         alert("Cashout successful! Your new balance is " + newBalance);
-        balanceElement.innerText = newBalance;
+        setBalance(newBalance);
     }
 
     else {
--- script/masine.js
+++ script/masine.js
@@ -1,7 +1,22 @@
-// id nie input value dibe
+// Masine id => input value 
 
 function getValueFromInput(id) {
     const input = document.getElementById(id);
     const value = input.value;
     return value;
+}
+
+//Masine => balance
+
+function getBalance() {
+    const balanceElement = document.getElementById("balance");
+    const balance = balanceElement.innerText;
+    return Number(balance);
+}
+
+// Masine value => stt balance
+
+function setBalance(value) {
+    const balanceElement = document.getElementById("balance");
+    balanceElement.innerText = value;
 }
